1652e42b7090e9d411bc1b14f3361055189a1500
rina-ns <rina-ns@leo.bbiq.jp>
2023-01-30 02:01:36 +0900

    models.pyとurls.pyのソース修正

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 #検索条件について定義したテーブル
 class Conditions(models.Model):
     condition_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey("User", to_field='id', null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
     category = models.CharField(default='null', max_length=150)
     comment = models.CharField(default='null', max_length=255)
 
--- Django_App/pairleary_app/urls.py
+++ Django_App/pairleary_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login, mypage, signupfunc
+from .views import login, mypage
 
 
 urlpatterns = [
-    path('signup/', signupfunc, name='signup'),
+    # path('signup/', signupfunc, name='signup'),
     path('login/', login, name='login'),
     path('mypage/', mypage, name='mypage'),
     
