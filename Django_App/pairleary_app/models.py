4fd8595b5195edf20fa81ab85fff94e2a7d504c6
nto300002 <samonkntd@gmail.com>
2023-01-29 22:59:49 +0900

    27_add: class Conditions detail-models.py

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -15,4 +15,11 @@ class Content(models.Model):
         return self.title
 
 class Conditions(models.Model):
-    pass
+    condition_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey("User", to_field='id', null=True)
+    category = models.CharField(default='null', max_length=150)
+    comment = models.CharField(default='null', max_length=255)
+
+    def __str__(self):
+        return self.condition_id
+
