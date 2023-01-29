b4eff9281e0ac307b037389b233d080c038934d1
nto300002 <samonkntd@gmail.com>
2023-01-29 23:16:08 +0900

    27_fix: testdata delete-models.py

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -4,16 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
     
-class Content(models.Model):
-    condition_id = models.IntegerField(primary_key=True)
-    user_id = models.CharField(default='null',max_length=255)
-    category = models.CharField(default='null',max_length=255)
-    comment = models.CharField(default='null',max_length=255)
-
-
-    def __str__(self):
-        return self.title
-
 #検索条件について定義したテーブル
 class Conditions(models.Model):
     condition_id = models.IntegerField(primary_key=True)
