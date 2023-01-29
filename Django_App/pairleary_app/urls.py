c8e14c2a1728dc24eae24c2f5077328a718e2b8e
GoOkumura <87104566+GoOkumura@users.noreply.github.com>
2023-01-29 21:28:51 +0900

    urls.pyにurlを記述

--- Django_App/pairleary_app/urls.py
+++ Django_App/pairleary_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login,mypage
+from .views import login, mypage, signupfunc
 
 
 urlpatterns = [
+    path('signup/', signupfunc, name='signup'),
     path('login/', login, name='login'),
     path('mypage/', mypage, name='mypage'),
-
 ]
