bd64ee3dcafbd147016c6340bdad928e4ba13d39
nto300002 <samonkntd@gmail.com>
2023-01-30 20:15:37 +0900

    add: Recordsテーブル作成(Ordersテーブルもマイグレーションに必要な部分のみ作成)

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -16,4 +16,11 @@ class Conditions(models.Model):
 
 #各種レコードについて定義したテーブル
 class Records(models.Model):
-    pass
+    record_id = models.IntegerField(primary_key=True)
+    order_id = models.ForeignKey('Orders', on_delete=models.SET_NULL, to_field='order_id', null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
+    condition_id = models.ForeignKey('Conditions', on_delete=models.SET_NULL, to_field='condition_id', null=True)
+
+#予約の日時についてのテーブル
+class Orders(models.Model):
+    order_id = models.IntegerField(primary_key=True)
