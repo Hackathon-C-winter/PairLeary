ad84efe4b2fc9805c43a1808d125dc7e65986662
rina-ns <rina-ns@leo.bbiq.jp>
2023-02-08 12:24:02 +0900

    テーブル変更、migration.shの修正

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -1,4 +1,3 @@
-
 # f,rom email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
@@ -8,35 +7,37 @@ from django.utils import timezone
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=100, verbose_name="ユーザー名", unique=True)
-    gender = models.CharField(max_length=1, choices=[(None, "--"), ("m", "男性"), ("f", "女性")], default=None, verbose_name="性別", blank=True, null=True)
+    gender_type = models.CharField(max_length=1, choices=[(None, "--"), ("m", "男性"), ("f", "女性")], default=None, verbose_name="性別", blank=True, null=True)
 
     def __str__(self):
         return self.username
     
 #検索条件について定義したテーブル
-class Conditions(models.Model):
-    condition_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
-    category = models.CharField(default='null', max_length=150)
-    comment = models.CharField(default='null', max_length=255)
+# class Conditions(models.Model):
+#     condition_id = models.IntegerField(primary_key=True)
+#     user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
+#     category = models.CharField(default='null', max_length=150)
+#     comment = models.CharField(default='null', max_length=255)
 
-    def __str__(self):
-        return self.condition_id
+#     def __str__(self):
+#         return self.condition_id
 
 #各種レコードについて定義したテーブル
-class Records(models.Model):
-    record_id = models.IntegerField(primary_key=True)
-    order_id = models.ForeignKey('Orders', on_delete=models.SET_NULL, to_field='order_id', null=True)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
-    condition_id = models.ForeignKey('Conditions', on_delete=models.SET_NULL, to_field='condition_id', null=True)
+# class Records(models.Model):
+#     record_id = models.IntegerField(primary_key=True)
+#     order_id = models.ForeignKey('Orders', on_delete=models.SET_NULL, to_field='order_id', null=True)
+#     user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True)
+#     condition_id = models.ForeignKey('Conditions', on_delete=models.SET_NULL, to_field='condition_id', null=True)
 
-#予約の日時についてのテーブル
+#予約テーブル
 class Orders(models.Model):
     order_id = models.IntegerField(primary_key=True)
-    condition_id = models.ForeignKey('Conditions', on_delete=models.SET_NULL, to_field='condition_id', null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True,related_name='user_id')
     order_date = models.DateTimeField(help_text='予約日付')
-    order_time_start = models.DateTimeField(help_text='予約開始時間')
-    order_time_end = models.DateTimeField(help_text='予約終了時間')
+    category = models.CharField(default='null', max_length=150)
+    hope_gender_type = models.CharField(max_length=1, choices=[(None, "--"), ("m", "男性"), ("f", "女性")], default=None, verbose_name="性別", blank=True, null=True)
+    comment = models.CharField(default='null', max_length=255)
+    matched_user_id = models.IntegerField(primary_key=False, default='null',null=True)
 
     def save(self, *args, **kwargs):
         self.order_date = timezone.now()  # 保存されるたびに更新
