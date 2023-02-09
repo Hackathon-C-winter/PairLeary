84bea7ffbc32096ccde1db6b180ca581958b8b40
rina-ns <rina-ns@leo.bbiq.jp>
2023-02-09 09:14:13 +0900

    migration.sh変更

--- .migration.sh
+++ .migration.sh
@@ -2,6 +2,6 @@
 
 # マイグレーションを実行
 docker compose -f docker-compose.yml exec app python manage.py makemigrations pairleary_app --noinput
-docker compose -f docker-compose.yml exec app python manage.py migrate pairleary_app --noinput
+docker compose -f docker-compose.yml exec app python manage.py migrate --noinput
 # スーパーユーザー作成
 # docker compose -f docker-compose.yml exec app python manage.py createsuperuser
