b36fc5020102ec9cac497edb568bb5ff71c4cc3c
rina-ns <rina-ns@leo.bbiq.jp>
2023-02-14 23:51:18 +0900

    渡す値をそのまま表示できるように修正

--- Django_App/pairleary_app/models.py
+++ Django_App/pairleary_app/models.py
@@ -8,7 +8,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=100, verbose_name="ユーザー名", unique=True)
     email = models.EmailField(max_length=100, verbose_name="メールアドレス", unique=True)
-    # gender_type = models.CharField(max_length=1, choices=[(None, "-"), ("m", "男性"), ("f", "女性")], default=None, verbose_name="性別", blank=True, null=True)
     gender_type = models.CharField(max_length=15, verbose_name="性別", blank=True, null=True)
 
     def __str__(self):
@@ -18,15 +17,12 @@ class UserProfile(models.Model):
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True,related_name='user_id')
-    # order_date = models.DateField(null=False,help_text='予約日付')
     order_date = models.DateField(null=False,help_text='予約日付')
     order_time_range_type = models.CharField(default='null',max_length=10)
     category = models.CharField(default='null', max_length=150)
-    # hope_gender_type = models.CharField(max_length=1, choices=[(None, "-"), ("m", "男性"), ("f", "女性")], default=None, verbose_name="性別", blank=True, null=True)
     hope_gender_type = models.CharField(max_length=15, default=None, verbose_name="性別", blank=True, null=True)
     comment = models.CharField(default='null', max_length=255)
     matched_user_id = models.ForeignKey(User, on_delete=models.SET_NULL,to_field='id', null=True,related_name='matched_user_id')
 
     def save(self, *args, **kwargs):
-        # self.order_date = timezone.now()  # 保存されるたびに更新
         return super(Orders, self).save(*args, **kwargs)
