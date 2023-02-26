82a4e5986dce654d41c5e29dd9b1067d3b7fd49b
sudatema <dy.ren.ray@gmail.com>
2023-02-27 01:55:29 +0900

    予約リストの色分け実装

--- Django_App/static/js/matching_list.js
+++ Django_App/static/js/matching_list.js
@@ -1,47 +1,48 @@
 // 予約リストのclass属性追加
 // カテゴリーの宣言
-const orderCategory = document.getElementById("add-class-name").innerHTML;
+const orderCategory = document.querySelector('.category').innerHTML;
 // const orderCategoryHopeGender = document.getElementById("add-class-name").innerHTML;
 
 // 目的・希望性別の条件分岐→classを追加
-const li = document.getElementById("add-class-name");
+const p = document.querySelector('.category');
 // 目的がコミュニケーションだったら
-if (orderCategory === "コミュニケーション"){
-    li.classList.add("develop-color");
+if (orderCategory === 'コミュニケーション'){
+    p.classList.add('ComColor');
 // 目的が開発目的だったら
 } else if (orderCategory === "開発") {
-    li.classList.add("develop-color");
+    p.classList.add('developColor');
 }
 
-const p = document.getElementById("add-class-name");
-    // 希望性別が男性だったら
-if (orderCategory === "男性") {
-    p.classList.add("gender-color");
-    // 希望性別が女性だったら
-} else if (orderCategory === "女性") {
-    p.classList.add("gender-color");
-}
-    // 希望性別がどちらでもOKだったら
 
-// if (orderCategory === "コミュニケーション" && orderCategory === "男性"){
-//     li.classList.add("develop-color");
-// } else if (orderCategory === "コミュニケーション" && orderCategory === "女性"){
-//     li.classList.add("develop-color");
-// } else if (orderCategory === "開発" && orderCategory === "男性"){
-//     li.classList.add("develop-color");
-// } else if (orderCategory === "開発" && orderCategory === "女性"){
-//     li.classList.add("develop-color");
+
+
+
+
+
+
+// 予約リストのclass属性追加
+// カテゴリーの宣言
+// const orderCategory = document.getElementById("addClassName").innerHTML;
+// const orderCategoryHopeGender = document.getElementById("add-class-name").innerHTML;
+
+// 目的・希望性別の条件分岐→classを追加
+// const p = document.getElementById("addClassName");
+// 目的がコミュニケーションだったら
+// if (orderCategory === "コミュニケーション"){
+//     p.classList.add("ComColor");
+// 目的が開発目的だったら
+// } else if (orderCategory === "開発") {
+//     p.classList.add("developColor");
 // }
 
 
+// const p = document.getElementById("add-class-name");
+    // 希望性別が男性だったら
+// if (orderCategory === "男性") {
+//     p.classList.add("gender-color");
+    // 希望性別が女性だったら
+// } else if (orderCategory === "女性") {
+//     p.classList.add("gender-color");
+// }
 
 
-
-
-
-
-
-// 予約のステータスの記述
-// classを追加
-// 予約のステータスがnullだったら
-// 予約のステータスがnullでなかったら
