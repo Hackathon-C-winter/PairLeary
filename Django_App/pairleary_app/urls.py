65c4ab2ec0fb60fdf8600a4ac5134f08b93afeed
rina-ns <rina-ns@leo.bbiq.jp>
2023-02-27 16:31:02 +0900

    tutorialをログイン必須に修正（関数ベースビューに変更）

--- Django_App/pairleary_app/urls.py
+++ Django_App/pairleary_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import loginfunc, mypage, create_order, search_matching,\
-      signupfunc, Tutorial, Header, logoutfunc
+      signupfunc, tutorial, Header, logoutfunc
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('order/', create_order, name='create_order'),
     path('matching/', search_matching, name='search_matching'),
     path('signup/', signupfunc, name='signup'),
-    path('tutorial/', Tutorial.as_view(), name='tutorial'),
+    path('tutorial/', tutorial, name='tutorial'),
     path('header/', Header.as_view(), name='header'),
     path('logout/', logoutfunc, name='logout')
 ]
