34bc41ed4677ea6fb0a90f76c16b815bc5f2d60a
sudatema <dy.ren.ray@gmail.com>
2023-03-01 10:43:33 +0900

    予約変更モーダルのHTML作成

--- Django_App/static/js/modal.js
+++ Django_App/static/js/modal.js
@@ -1,11 +1,22 @@
-// tutorialのモーダルのJS指定
+// モーダルのJS指定
+
+// 予約編集・削除のモーダルを開く記述
 const modalBtns = document.querySelectorAll(".modalOpen");
+modalBtns.forEach(function (btn) {
+    btn.onclick = function () {
+    const modal = btn.getAttribute('data-modal');
+        document.getElementById(`${modal}-${order_id}`).style.display = "block";
+    };
+});
+// 上記以外のモーダルを開く記述
+const openModalBtns = document.querySelectorAll(".listModalOpen");
 modalBtns.forEach(function (btn) {
     btn.onclick = function () {
     const modal = btn.getAttribute('data-modal');
         document.getElementById(modal).style.display = "block";
     };
 });
+
 
 // Xボタンを押したらモーダルを閉じる
 const closeBtns = document.querySelectorAll(".modalClose");
@@ -31,3 +42,29 @@ window.onclick = function (event) {
     }
 };
 
+
+
+
+// // Xボタンを押したらモーダルを閉じる
+// const closeBtns = document.querySelectorAll(".modalClose");
+// closeBtns.forEach(function (btn) {
+//     btn.onclick = function () {
+//     const modal = btn.closest('.modal');
+//     modal.style.display = "none";
+//     };
+// });
+
+// // キャンセルボタンを押したらモーダルを閉じる
+// const cancelBtns = document.querySelectorAll(".cancelButton");
+// cancelBtns.forEach(function (btn) {
+//     btn.onclick = function () {
+//     const modal = btn.closest('.modal');
+//     modal.style.display = "none";
+//     };
+// });
+// // モーダル以外をクリックしたら閉じる
+// window.onclick = function (event) {
+//     if (event.target.className === "modal") {
+//     event.target.style.display = "none";
+//     }
+// };
