8388f0e73de5b766fd2f83aca4915589e3cbff35
hinakana777 <gtt2938hinakana@icloud.com>
2023-03-03 01:41:11 +0900

    本番環境

--- .docker_clear.sh
+++ .docker_clear.sh
@@ -8,3 +8,15 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 # Dockerのシステムなどを削除
 docker system prune
+
+#!/bin/sh
+
+# Dockerコンテナを一括で停止・削除
+#docker compose down -v
+docker compose -f docker-compose.prod.yml down -v
+# 停止中のDockerコンテナを一括で削除
+docker rm $(docker ps -a -q)
+# Dockerイメージを一括で削除
+docker rmi $(docker images -q)
+# Dockerのシステムなどを削除
+docker system prune
